619cc53e1dd3f21ae84418c40ad05136bdb98bd2
kumarnani25 <kumar.nani25@gmail.com>
2019-02-10 23:06:34 +0530

    commit

--- practisejenkins/src/main/java/com/demo/learning/invokebrowser.java
+++ practisejenkins/src/main/java/com/demo/learning/invokebrowser.java
@@ -3,19 +3,36 @@ package com.demo.learning;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class invokebrowser {
 	
 	public WebDriver driver;
 	
+	
+	@Parameters({"browser","url"})
 	@Test
-	public void test() throws InterruptedException
+	public void test(String browser,String url) throws InterruptedException
 	{
+		
+		if(browser.equals("chrome"))
+		{
 		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
 		 driver=new ChromeDriver();
-		driver.get("https://opensource-demo.orangehrmlive.com/");
+		}
+		if(browser.equals("IE"))
+		{
+			System.out.println("ie is invoked");
+			System.setProperty("webdriver.ie.driver", "D:\\IEDriverServer.exe");
+			 driver=new InternetExplorerDriver();
+		}
+		 
+		 //https://opensource-demo.orangehrmlive.com/
+		 
+		driver.get(url);
 		Thread.sleep(5000);
 		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
 		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
